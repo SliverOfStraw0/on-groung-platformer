aeed793379f50604e29ac56e1d58b31ecdadd9fb
A-lox <simnmickal@gmail.com>
2025-02-18 13:52:20 -0500

    added wallJumping

--- scripts/main_player.gd
+++ scripts/main_player.gd
@@ -1,20 +1,25 @@
 extends CharacterBody2D
 
-@export var acc = 200
-@export var drag = 0.025
-
-@export var defMaxSpeed = 900
+@export var acc = 400
+@export var drag = 0.15
+@export var defMaxSpeed = 650
 var maxSpeed = defMaxSpeed
-
 
 
 @export var maxJumps = 1
 @export var currentJump = 0
+@export var JUMP_VELOCITY = 50000.0
+@export var JMinscrease = 0.1
+@export var defJumpMultiplyer = 1.5
+var jumpMultiplyer = defJumpMultiplyer
+@export var walljumpdisplace = 1500
+var canSlamJump = false
+var isOnJumpableWall = false
+
 
 @export var defultGrav = 2500
 var gravity = defultGrav
 
-@export var JUMP_VELOCITY = 50000.0
 
 
 
@@ -27,6 +32,7 @@ func _physics_process(delta: float) -> void:
 		currentJump = 0
 		gravity = defultGrav
 
+	
 	move_and_slide()
 	
 	
@@ -48,14 +54,46 @@ func basicMovement(delta):
 	
 	if Input.is_action_just_pressed("ui_accept") and currentJump < maxJumps:
 		currentJump += 1
-		velocity.y = -JUMP_VELOCITY * delta
+		if isOnJumpableWall:
+			velocity.x = walljumpdisplace*get_wall_normal().x
+		
+		if !canSlamJump:
+			velocity.y = -JUMP_VELOCITY * delta
+		else:
+			velocity.y = ((-JUMP_VELOCITY)*jumpMultiplyer) * delta
+			jumpMultiplyer += JMinscrease
 		
 func slideAndStopMovement(delta):
 	if Input.is_action_just_pressed("slideStomp"):
 		if !$RayCast2D.is_colliding():
 			gravity = 50000
+			canSlamJump = true
+			$"time fore higher jump after slam".start()
 		else:
 			gravity = defultGrav
 			maxSpeed = 2000
 	elif Input.is_action_just_released("slideStomp"):
 		maxSpeed = defMaxSpeed
+		
+
+func _on_time_fore_higher_jump_after_slam_timeout() -> void:
+	canSlamJump = false
+	jumpMultiplyer = defJumpMultiplyer
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	pass
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("wallJump"):
+		#print("isOnWallGroup")
+		if is_on_wall():
+			currentJump = 0
+			isOnJumpableWall = true
+			#print("PrevAndIsOnWALL")
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	isOnJumpableWall = false
+	
