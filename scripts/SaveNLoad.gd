75dbdac096d5e5cde4caaaca0ec0125110ec2645
A-lox <simnmickal@gmail.com>
2025-03-04 19:51:09 -0500

    updated node: change cam view restrictions

--- scripts/SaveNLoad.gd
+++ scripts/SaveNLoad.gd
@@ -1,8 +1,6 @@
-extends Area2D
+extends Node
 
-@export_category("cam limits")
-@export var minX = 0
-@export var maxX = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -11,9 +9,3 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
-func _on_area_entered(area: Area2D) -> void:
-	print("AREA ENTERS TO CHANGE CAM")
-	Globals.emit_signal("changeCamLimits",minX,maxX)
-	
